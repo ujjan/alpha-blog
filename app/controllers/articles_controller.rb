653b93cf66363d801e25912905d255c125137ad8
Ayaz Ali <ayazali@Ayazs-MacBook-Pro.local>
2017-08-08 13:59:10 +0200

    some more changes

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,16 @@
+class ArticlesController < ApplicationController
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :discription)
+    end
+end
